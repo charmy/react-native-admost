07c1f170b766a0907428c3a38dc99d9ec4542fc7
srfaytkn <serefaytekinn@gmail.com>
2020-10-20 14:48:21 +0300

    admost events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import { NativeModules } from "react-native";
-
-const { AdMost } = NativeModules;
-
+import { NativeModules, NativeEventEmitter } from "react-native";
 export AdMostAdView from "./AdMostAdView";
+
+export const { AdMost, AdMostInterstitial, AdMostRewarded } = NativeModules;
+export const AdMostEventEmitter = new NativeEventEmitter(AdMost);
+
+export const BannerAdEvents = {
+    ON_READY: "ADMOST_BANNER_ON_READY",
+    ON_FAIL: "ADMOST_BANNER_ON_FAIL",
+    ON_CLICK: "ADMOST_BANNER_ON_CLICK",
+};
+
+export const InterstitialAdEvents = {
+    ON_READY: "ADMOST_INTERSTITIAL_ON_READY",
+    ON_FAIL: "ADMOST_INTERSTITIAL_ON_FAIL",
+    ON_DISMISS: "ADMOST_INTERSTITIAL_ON_DISMISS",
+    ON_COMPLETE: "ADMOST_INTERSTITIAL_ON_COMPLETE",
+    ON_SHOWN: "ADMOST_INTERSTITIAL_ON_SHOWN",
+    ON_CLICKED: "ADMOST_INTERSTITIAL_ON_CLICKED",
+    ON_STATUS_CHANGED: "ADMOST_INTERSTITIAL_ON_STATUS_CHANGED",
+};
+
+export const RewardedAdEvents = {
+    ON_READY: "ADMOST_REWARDED_ON_READY",
+    ON_FAIL: "ADMOST_REWARDED_ON_FAIL",
+    ON_DISMISS: "ADMOST_REWARDED_ON_DISMISS",
+    ON_COMPLETE: "ADMOST_REWARDED_ON_COMPLETE",
+    ON_SHOWN: "ADMOST_REWARDED_ON_SHOWN",
+    ON_CLICKED: "ADMOST_REWARDED_ON_CLICKED",
+    ON_STATUS_CHANGED: "ADMOST_REWARDED_ON_STATUS_CHANGED",
+};
+
 export default AdMost;
